56d2e5e56f2288c31d23e77d4d25b4fa89b4f694
Sreejanee <mandalsreejanee2002@gmail.com>
2023-07-28 18:04:27 +0530

    Initial SetUp done.The Routes for Home and Create post page set

---
+++ Frontend/src/pages/Createpost.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Createpost = () => {
+  return (
+    <div>
+      CreatePost
+    </div>
+  )
+}
+
+export default Createpost
---
+++ Frontend/src/pages/index.js
@@ -0,0 +1,5 @@
+import Home from "./Home";
+import Createpost from "./Createpost";
+export{
+    Home,Createpost
+}
